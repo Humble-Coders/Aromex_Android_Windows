066fb2e6bab4e54fab6964c508c07aec9b597ab5
shivinders12 <singhshivinder.12@gmail.com>
2026-01-06 21:28:40 +0530

    First Android Commit

--- composeApp/src/androidMain/kotlin/com/humblecoders/aromex_android_windows/presentation/ui/AndroidHomeScreen.kt
+++ composeApp/src/androidMain/kotlin/com/humblecoders/aromex_android_windows/presentation/ui/AndroidHomeScreen.kt
@@ -64,7 +64,7 @@ fun AndroidHomeScreen(
             viewModel.dismissEntitySuccess()
         }
     }
-
+    ;
     ModalNavigationDrawer(
         drawerState = drawerState,
         drawerContent = {
